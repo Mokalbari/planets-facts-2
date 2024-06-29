5d633155db759950ea53c7ace967b0068ea2dd29
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-29 18:23:52 +0200

    add NavBar component + planetList array from data

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import '../styles/NavBar.css'
+import planetList from '../data/planetList'
+
+function NavBar() {
+  return (
+    <header className='header flex-row'>
+      <span className='header__logo'>The planets</span>
+      <nav className='header__nav'>
+        <menu className='header__menu flex-row'>
+          {planetList.map(planet => (
+            <li className='header__list-item header-3' key={planet.name}>
+              {planet.name}
+            </li>
+          ))}
+        </menu>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
---
+++ src/data/planetList.ts
@@ -0,0 +1,5 @@
+import data from './data'
+
+const planetList = Object.values(data)
+
+export default planetList
