adeadf0cfbb1d6aba887e8407432e5e05f01b744
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-29 19:53:00 +0200

    add information section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import NavBar from "./components/NavBar"
 import MainContainer from "./components/MainContainer"
+import Information from "./components/Information.tsx"
 
 function App() {
   return (
     <>
       <NavBar />
       <MainContainer />
+      <Information />
     </>
   )
 }
