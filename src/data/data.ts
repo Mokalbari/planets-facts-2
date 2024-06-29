142fdb1e65c6edfc3de7106bac1564fda524f25c
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-29 17:07:04 +0200

    Init depot

---
+++ src/data/data.ts
@@ -0,0 +1,31 @@
+import mercury from '../assets/planet-mercury.svg'
+import mercuryInternal from '../assets/planet-mercury-internal.svg'
+import geologyMercury from '../assets/geology-mercury.png'
+
+import mercury from '../assets/planet-mercury.svg'
+import venusInternal from '../assets/planet-venus-internal.svg'
+import geologyVenus from '../assets/geology-venus.png'
+
+import earth from '../assets/planet-earth.svg'
+import earthInternal from '../assets/planet-earth-internal.svg'
+import geologyEarth from '../assets/geology-earth.png'
+
+import mars from '../assets/planet-mars.svg'
+import marsInternal from '../assets/planet-mars-internal.svg'
+import geologyMars from '../assets/geology-mars.png'
+
+import jupiter from '../assets/planet-jupiter.svg'
+import jupiterInternal from '../assets/planet-jupiter-internal.svg'
+import geologyJupiter from '../assets/geology-jupiter.png'
+
+import saturn from '../assets/planet-saturn.svg'
+import saturnInternal from '../assets/planet-saturn-internal.svg'
+import geologySaturn from '../assets/geology-saturn.png'
+
+import uranus from '../assets/planet-uranus.svg'
+import uranusInternal from '../assets/planet-uranus-internal.svg'
+import geologyUranus from '../assets/geology-uranus.png'
+
+import neptune from '../assets/planet-neptune.svg'
+import neptuneInternal from '../assets/planet-neptune-internal.svg'
+import geologyNeptune from '../assets/geology-neptune.png'
