2c3b51b0b54c71a2cb02df0dc905584bf44694cd
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-06-29 17:30:20 +0200

    add data types + types Planet to hold the object types

---
+++ src/types/Planet.ts
@@ -0,0 +1,10 @@
+interface Planet {
+  name: string
+  informations: { content: string; source: string }[]
+  rotation: string
+  revolution: string
+  radius: string
+  temperature: string
+}
+
+export default Planet
